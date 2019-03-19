ab595e920b97b661449194f5876b7d20c2aaf890
Andrew Wanex <awanex2705@gmail.com>
2019-03-19 16:34:26 -0700

    refactor: update security camera player and controls to new process methods and updated server access

--- src/pages/sec-cam/sec-cam.ts
+++ src/pages/sec-cam/sec-cam.ts
@@ -1,29 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ViewController, ModalController, PopoverController } from 'ionic-angular';
 import { Subject } from 'rxjs/Subject';
 
-import { baseURL } from '../../shared/baseurl';
-import { apiVersion } from '../../shared/apiVersion';
+import { videoBaseURL } from '../../shared/videourl';
+import { VideoMetaData } from '../../shared/videodata';
 
 import { WebsocketConnectionProvider } from '../../providers/websocket-connection/websocket-connection';
 import { SecCamProvider } from '../../providers/sec-cam/sec-cam';
 import { AuthenticationProvider } from '../../providers/authentication/authentication';
 
-@IonicPage()
+import { VideoQueryFormPage } from '../video-query-form/video-query-form';
+import { VideoOptionsPopoverPage } from '../video-options-popover/video-options-popover';
+
 @Component({
   selector: 'page-sec-cam',
   templateUrl: 'sec-cam.html',
 })
 export class SecCamPage implements OnInit {
 
-  private videoNameList: String[] = [];
+  private videoNameList: VideoMetaData[] = [];
+  private selectedVideo: VideoMetaData = null;
   private isRecording = false;
   private classRecording = '';
   private recordDisplay = 'Record';
-  // TODO update for dynamic location name
-  private locationName = 'Front-door';
   private recordingProcessing = false;
-  private videoBaseURL: String = `${baseURL}${apiVersion}security/seccam/`;
   private videoURL: String = '';
   private _unsubscribe: Subject<void> = new Subject<void>();
   private socket;
@@ -32,16 +32,19 @@ export class SecCamPage implements OnInit {
     public navParams: NavParams,
     private secCamService: SecCamProvider,
     private wssConnection: WebsocketConnectionProvider,
-    private authService: AuthenticationProvider) { }
+    private authService: AuthenticationProvider,
+    public viewCtrl: ViewController,
+    public modalCtrl: ModalController,
+    public popoverCtrl: PopoverController) { }
 
   ngOnInit() {
-    this.initCameraFiles();
     try {
       this.wssConnection.getSocket()
         .takeUntil(this._unsubscribe)
         .subscribe(socket => {
           console.log('Security viewer connection to socket established');
           this.socket = socket;
+
           // socket listener for security cameras
           this.secCamService.listenForSecCamData(socket)
             .takeUntil(this._unsubscribe)
@@ -49,23 +52,10 @@ export class SecCamPage implements OnInit {
               console.log('Incoming data from server');
               this.handleWebsocketData(data);
             });
-          // setTimeout(() => {
-          //   this.secCamService.getCameraFileNames();
-          // }, 1000);
         });
     } catch(e) {
       console.log('Socket connection error', e);
     }
-  }
-
-  initCameraFiles() {
-    this.secCamService.getCameraFileNames()
-      .subscribe(filenames => {
-        console.log(filenames);
-        this.videoNameList = filenames.reverse();
-        this.selectVideo(this.videoNameList[0]);
-        // this.videoURL = `${this.videoBaseURL}${this.videoNameList[0]}`;
-      }, err => console.log(err));
   }
 
   ionViewDidLoad() {
@@ -82,25 +72,42 @@ export class SecCamPage implements OnInit {
     const result = data.data;
     switch (method) {
       case 'video-list':
-        console.log('Stream data available');
-        this.videoNameList = result.reverse();
-        this.selectVideo(this.videoNameList[0]);
+        console.log('Stream data available', result);
+        this.videoNameList = result;
+        if (result.length) {
+          this.selectVideo(this.videoNameList[0].filename, 0);
+        }
+        break;
+      case 'new-video':
+        if (result) {
+          this.videoNameList.unshift(result);
+        }
         this.recordingProcessing = false;
         break;
+      case 'updated-metadata':
+        if (result) {
+          const toUpdate = this.videoNameList.findIndex(index => result._id == index._id);
+          console.log(toUpdate);
+          this.videoNameList[toUpdate] = result;
+          this.selectedVideo = result;
+        }
+      case 'video-deleted':
+        // TODO handle updating lists after file was deleted from server
+        break;
       case 'error':
-        // this.errMsg = result;
+        // TODO add error feedback handling
         console.log('Encountered an error', result);
         break;
       default:
-        console.log('Supplied method is not available for CLIMATE, this is not an error');
+        console.log('Supplied method is not available for SECURITY CAMERA, this is not an error');
         break;
     }
   }
 
-  selectVideo(event) {
-    console.log(event);
+  selectVideo(filename, index) {
+    this.selectedVideo = this.videoNameList[index];
     const token = this.authService.getToken();
-    this.videoURL = `${this.videoBaseURL}${event}/?vidauth=${token}`;
+    this.videoURL = `${videoBaseURL}${filename}/?vidauth=${token}`;
   }
 
   toggleRecording() {
@@ -112,6 +119,34 @@ export class SecCamPage implements OnInit {
     this.recordDisplay = this.isRecording ? 'Stop Recording': 'Record';
     this.secCamService.toggleRecording(this.isRecording);
     console.log(`Recording has ${this.isRecording ? 'started': 'ended'}`);
+  }
+
+  toggleStarred() {
+    this.secCamService.toggleStarred(this.selectedVideo._id, !this.selectedVideo.starred);
+  }
+
+  openQueryModal() {
+    const modal = this.modalCtrl.create(VideoQueryFormPage);
+    modal.onDidDismiss(data => {
+      if (data) {
+        this.secCamService.getVideoFilenames(data);
+      }
+    });
+    modal.present();
+  }
+
+  openOptionsPopover() {
+    const popover = this.popoverCtrl.create(
+      VideoOptionsPopoverPage,
+      {list: this.videoNameList},
+      {cssClass: 'video-options-popover'}
+    );
+    popover.onDidDismiss(data => {
+      if (data && data.filename) {
+        this.selectVideo(data.filename, data.index);
+      }
+    });
+    popover.present();
   }
 
 }
