cd04c1424ee945566bf87b7c98df132617035cd5
Andrew Wanex <awanex2705@gmail.com>
2019-03-19 16:39:25 -0700

    feat: add video metadata interface

---
+++ src/shared/videodata.ts
@@ -0,0 +1,10 @@
+export interface VideoMetaData {
+  _id: string,
+  filename: number,
+  location: string,
+  startDatetime: number,
+  endDateTime: number,
+  duration: number,
+  triggerEvent: string,
+  starred: boolean
+};
