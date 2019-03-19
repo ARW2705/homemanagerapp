8eabfdd5d2ebdb5cf189999f5fc164eb67255c3a
Andrew Wanex <awanex2705@gmail.com>
2019-03-19 16:37:10 -0700

    refactor: update security camera service handling to new process

--- src/providers/sec-cam/sec-cam.ts
+++ src/providers/sec-cam/sec-cam.ts
@@ -7,14 +7,11 @@ import 'rxjs/add/operator/catch';
 import { baseURL } from '../../shared/baseurl';
 import { apiVersion } from '../../shared/apiVersion';
 
+import { VideoMetaData } from '../../shared/videodata';
+
 import { ProcessHttpmsgProvider } from '../process-httpmsg/process-httpmsg';
 
-/*
-  Generated class for the SecCamProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class SecCamProvider {
 
@@ -32,14 +29,47 @@ export class SecCamProvider {
       console.log('Websocket failed to connect to server');
       return;
     }
+    this.getVideoFilenames({startDateTime: '', endDateTime: '', limit: 12});
     console.log('Listening for seccam data');
     return new Observable(obs => {
       this.socket.on('response-get-video-list', data => {
-        if (data.err) {
+        console.log('get video response');
+        if (data.error) {
           this.payload.type = 'error';
-          this.payload.data = data.err;
+          this.payload.data = data.error;
         } else {
           this.payload.type = 'video-list';
+          this.payload.data = data.data;
+        }
+        obs.next(this.payload);
+      });
+      this.socket.on('proxy-request-notify-new-video-available', data => {
+        console.log('new video available');
+        if (data.error) {
+          this.payload.type = 'error';
+          this.payload.data = data.error;
+        } else {
+          this.payload.type = 'new-video';
+          this.payload.data = data.data;
+        }
+        obs.next(this.payload);
+      });
+      this.socket.on('response-update-video-data', data => {
+        if (data.error) {
+          this.payload.type = 'error';
+          this.payload.data = data.error;
+        } else {
+          this.payload.type = 'updated-metadata';
+          this.payload.data = data.data;
+        }
+        obs.next(this.payload);
+      });
+      this.socket.on('response-delete-video', data => {
+        if (data.error) {
+          this.payload.type = 'error';
+          this.payload.data = data.error;
+        } else {
+          this.payload.type = 'video-deleted';
           this.payload.data = data.data;
         }
         obs.next(this.payload);
@@ -59,19 +89,27 @@ export class SecCamProvider {
    - shutdown camera
   **/
 
-  getCameraFileNames(): Observable<any> {
-    console.log('getting video file names');
-    // this.socket.emit('request-get-video-list', {});
-    return this.http.get(baseURL + apiVersion + 'security/seccam')
-      .catch(err => this.processHttpMsgService.handleError(err));
-  }
-
   toggleRecording(record) {
     this.socket.emit('request-stream', {stream: record});
   }
 
-  getFileByName() {
+  getVideoFilenames(query) {
+    const formattedQuery = {};
+    formattedQuery['startDateTime'] = {
+      $gte: (query.startDateTime == '') ? new Date('01 January 2010 00:00 UTC').toISOString(): query.startDateTime,
+      $lte: (query.endDateTime == '') ? new Date().toISOString(): query.endDateTime
+    };
+    if (query.event != 'all') {
+      formattedQuery['tiggerEvent'] = query.event;
+    }
+    if (query.starred) {
+      formattedQuery['starred'] = true;
+    }
+    this.socket.emit('request-get-videos-by-params', {query: formattedQuery, limit: query.limit});
+  }
 
+  toggleStarred(id: string, starred: boolean) {
+    this.socket.emit('request-update-video-data', {id: id, starred: starred});
   }
 
 }
